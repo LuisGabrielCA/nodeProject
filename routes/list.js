296fd19e2b514d4abf0b171a65297e090f451329
LuisGabrielCA <luuisgca@gmail.com>
2020-09-07 20:57:47 -0300

    test

--- routes/list.js
+++ routes/list.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const cadastro = require('../models/cadastro')
 
-router.get('/listUser/:idusuarios', function (req, res) {
-  const user = cadastro.findAll({
+router.get('/listUser/:idusuarios', async function (req, res) {
+  const user = await cadastro.findAll({
     where: {
       idusuarios: req.params.idusuarios
     }
